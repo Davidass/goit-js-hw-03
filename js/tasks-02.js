72d42ff8b550b462cc3531d80c4e17ab3ebacc56
Davidass <gidrig@gmail.com>
2020-05-16 00:18:22 +0300

    tasks-02

--- js/tasks-02.js
+++ js/tasks-02.js
@@ -1,30 +1,20 @@
-const calculateEngravingPrice = function(message, pricePerWord) {
-  const arrWord = message.split(' ');
-  const totalPrice = arrWord.length * pricePerWord;
-  return totalPrice;
+const countProps = function(obj) {
+  // const values = Object.values(obj);
+  // let totalValues = 0;
+
+  // // eslint-disable-next-line no-restricted-syntax
+  // for (const value of values) {
+  //   totalValues += 1;
+  // }
+  // return totalValues;
+  return Object.values(obj).length;
 };
 
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-console.log(
-  calculateEngravingPrice(
-    'Proin sociis natoque et magnis parturient montes mus',
-    10,
-  ),
-); // 80
+console.log(countProps({})); // 0
 
-console.log(
-  calculateEngravingPrice(
-    'Proin sociis natoque et magnis parturient montes mus',
-    20,
-  ),
-); // 160
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
 
-console.log(
-  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-); // 240
-
-console.log(
-  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-); // 120
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
