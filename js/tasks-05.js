976d55286aeead8e93ce05bba15e72fda4226be3
Davidass <gidrig@gmail.com>
2020-05-20 22:50:45 +0300

    change tasks-05

--- js/tasks-05.js
+++ js/tasks-05.js
@@ -1,5 +1,3 @@
-// Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства. Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
-
 const products = [
   { name: 'Радар', price: 1300, quantity: 4 },
   { name: 'Сканер', price: 2700, quantity: 3 },
@@ -12,7 +10,7 @@ const getAllPropValues = function(arr, prop) {
 
   // eslint-disable-next-line
   for (const entry of arr) {
-    if (entry[prop] !== undefined) {
+    if (entry[prop]) {
       array.push(entry[prop]);
     }
   }
